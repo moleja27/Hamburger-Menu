9bb3c71938ce7b164c5c56891a3796d66dcffd54
Alejandra <monicsalejandra@gmail.com>
2024-03-15 12:05:09 +0100

    it was added JS

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("¡Bienvenidos!");
+
+function myFunction() {
+    var x = document.getElementById("mynav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
